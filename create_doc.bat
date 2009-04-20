0a6ec768c819d8f2dea1d2816b9a9fa3c213a546
benishouga <benishouga@gmail.com>
2009-04-21 01:41:34 +0900

    bat edit

--- create_doc.bat
+++ create_doc.bat
@@ -1,2 +1,2 @@
-java -jar C:\soft\jsdoc-toolkit\jsrun.jar C:/soft/jsdoc-toolkit/app/run.js  -t=C:/soft/jsdoc-toolkit/templates/jsdoc -d=doc ".\httpreqmock.js"
+java -jar C:\soft\jsdoc-toolkit\jsrun.jar C:/soft/jsdoc-toolkit/app/run.js  -t=C:/soft/jsdoc-toolkit/templates/jsdoc -d="httpreqmock_doc_jp" ".\httpreqmock.js"
 pause
